d42badb7e560528d14468f95695c68a37d27ef0c
swaitankjha <swaitankj@gmail.com>
2026-01-11 21:54:41 +0530

    Convert frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Dashboard from "./components/Dashboard";
+import { Toaster } from "./components/ui/sonner";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const API = `${BACKEND_URL}/api`;
+
+const Home = () => {
+  const helloWorldApi = async () => {
+    try {
+      const response = await axios.get(`${API}/`);
+      console.log(response.data.message);
+    } catch (e) {
+      console.error(e, `errored out requesting / api`);
+    }
+  };
+
+  useEffect(() => {
+    helloWorldApi();
+  }, []);
+
+  return <Dashboard />;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
